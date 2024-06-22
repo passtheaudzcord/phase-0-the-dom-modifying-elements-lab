efddb9b1842cd0cfd92f905867711c34551261c8
Audrey <chipmanaudrey@gmail.com>
2024-06-22 15:50:22 -0700

    Learn more about dom and modify elements

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+  //removeAttribute('id');
+  //body.removeChild('id');
+
+  const element = document.getElementById("main");
+  element.remove();
+
+  const newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+
+  newHeader.textContent = "AUDREY is the champion";
